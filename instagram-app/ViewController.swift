cd8f27fb28ef16c9812ecd05992b595195185c30
Aswin Raj Kharel <query.ark@gmail.com>
2018-09-22 18:37:51 -0500

    Initial Commit

---
+++ instagram-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram-app
+//
+//  Created by Aswin Raj Kharel on 9/22/18.
+//  Copyright © 2018 Aswin Raj Kharel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
