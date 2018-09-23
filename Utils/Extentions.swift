4e759436692fbffe10a9bab702fccc5c06f2a84c
Aswin Raj Kharel <query.ark@gmail.com>
2018-09-22 20:58:22 -0500

    UIColor and Anchor Extension

---
+++ Utils/Extentions.swift
@@ -0,0 +1,41 @@
+//
+//  Extentions.swift
+//  instagram-app
+//
+//  Created by Aswin Raj Kharel on 9/22/18.
+//  Copyright © 2018 Aswin Raj Kharel. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView{
+    func anchor(top: NSLayoutYAxisAnchor?, paddingTop: CGFloat, left: NSLayoutXAxisAnchor?, paddingLeft: CGFloat, bottom: NSLayoutYAxisAnchor?, paddingBottom: CGFloat, right: NSLayoutXAxisAnchor?, paddingRight: CGFloat, width: CGFloat, height: CGFloat){
+        self.translatesAutoresizingMaskIntoConstraints = false
+        if let top = top{
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let left = left{
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let bottom = bottom{
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        if let right = right{
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if width != 0 {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if height != 0{
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    
+}
--- instagram-app/ViewController.swift
+++ instagram-app/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UIViewController {
     var emailTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Email"
-        tf.translatesAutoresizingMaskIntoConstraints = false
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.font = UIFont.systemFont(ofSize: 14)
         tf.borderStyle = .roundedRect
@@ -30,7 +29,6 @@ class ViewController: UIViewController {
     var usernameTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Username"
-        tf.translatesAutoresizingMaskIntoConstraints = false
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.font = UIFont.systemFont(ofSize: 14)
         tf.borderStyle = .roundedRect
@@ -40,7 +38,6 @@ class ViewController: UIViewController {
     var passwordTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Password"
-        tf.translatesAutoresizingMaskIntoConstraints = false
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.font = UIFont.systemFont(ofSize: 14)
         tf.borderStyle = .roundedRect
@@ -51,6 +48,7 @@ class ViewController: UIViewController {
         let button = UIButton(type: .system)
         button.setTitle("Sign Up", for: .normal)
         button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
@@ -67,10 +65,11 @@ class ViewController: UIViewController {
     func setupUI(){
         // Add the plus photo button
         self.view.addSubview(self.plusPhotoButton)
-        self.plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
-        self.plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+//        self.plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+//        self.plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         self.plusPhotoButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        self.plusPhotoButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40).isActive = true
+//        self.plusPhotoButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40).isActive = true
+        self.plusPhotoButton.anchor(top: self.view.topAnchor, paddingTop: 40, left: nil, paddingLeft: 0, bottom: nil, paddingBottom: 0, right: nil, paddingRight: 0, width: 140, height: 140)
         
         setupInputFields()
 //        self.view.addSubview(self.emailTextField)
@@ -83,17 +82,12 @@ class ViewController: UIViewController {
     fileprivate func setupInputFields(){
 
         let stackView = UIStackView(arrangedSubviews: [self.emailTextField, self.usernameTextField, self.passwordTextField, self.signUpButton])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.distribution = .fillEqually
         stackView.axis = .vertical
         stackView.spacing = 10
     
         view.addSubview(stackView)
-        stackView.topAnchor.constraint(equalTo: self.plusPhotoButton.bottomAnchor, constant: 20).isActive = true
-        stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        stackView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 40).isActive = true
-        stackView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -40).isActive = true
-        
+        stackView.anchor(top: self.plusPhotoButton.bottomAnchor, paddingTop: 20, left: self.view.leftAnchor, paddingLeft: 40, bottom: nil, paddingBottom: 0, right: self.view.rightAnchor, paddingRight: 40, width: 0, height: 200)
     }
     
     override func didReceiveMemoryWarning() {
